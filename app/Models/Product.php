719349cdfbf58a55543dab42770eba466b6bed80
prafull <prafullvyas272@gmail.com>
2025-10-01 17:55:28 +0530

    Product filters

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,10 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Customer;
+use App\Models\ProductCategory;
 
 class Product extends Model
 {
     use HasFactory;
 
     protected $guarded = ['id', 'created_at', 'updated_at'];
+
+    public function assignedTo()
+    {
+        return $this->belongsTo(Customer::class, 'assigned_to');
+    }
+
+    public function productCategory()
+    {
+        return $this->belongsTo(ProductCategory::class, 'product_category_id');
+    }
 }
